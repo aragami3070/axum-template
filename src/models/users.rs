ec7c2396ce4cea88080848b87478631a85612da6
aragami3070 <smirnov17612@gmail.com>
2026-04-09 22:16:41 +0400

    feat(models): add Role enum for User

--- src/models/users.rs
+++ src/models/users.rs
@@ -5,6 +5,15 @@ use uuid::Uuid;
 pub struct User {
     pub id: Uuid,
     pub name: String,
+    pub email: String,
+    pub role: Role,
     #[serde(skip_serializing)]
-    pub password_hash: String
+    pub password_hash: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, sqlx::Type)]
+#[sqlx(type_name = "text", rename_all = "lowercase")]
+pub enum Role {
+    User,
+    Admin,
 }
