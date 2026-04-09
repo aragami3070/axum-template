af7aa53951d186bfb11d0372fb803d30ec5e261e
aragami3070 <smirnov17612@gmail.com>
2026-04-09 21:56:50 +0400

    feat(repository): add UserRepository trait

--- src/repository/users.rs
+++ src/repository/users.rs
@@ -1,8 +1,18 @@
 use macroses::NewTypeDeref;
 use std::ops::Deref;
+use uuid::Uuid;
+
+use crate::models::users::User;
 
 #[derive(NewTypeDeref)]
 pub struct Limit(pub u32);
 #[derive(NewTypeDeref)]
 pub struct Offset(pub u32);
+
+pub trait UserRepository {
+    async fn get_by_id(&self, id: &Uuid) -> Option<User>;
+    async fn get(&self, offset: &Offset, limit: &Limit) -> Option<User>;
+    async fn create(&self, user: User);
+    async fn update(&self, user: User);
+}
 
