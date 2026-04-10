a091541641a4537b86845f09ccce19643ee35bf3
aragami3070 <smirnov17612@gmail.com>
2026-04-10 11:14:59 +0400

    feat(repository): add is_unique_violation error checker

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -1,1 +1,11 @@
 pub mod users;
+
+/// Является ли ошибка вставкой уже существующей строки?
+pub fn is_unique_violation(err: &sqlx::Error) -> bool {
+    match err {
+        sqlx::Error::Database(db_err) => {
+            db_err.code().as_deref() == Some("23505")
+        }
+        _ => false,
+    }
+}
